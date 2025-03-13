8e578b15f7dd56e85771e4bc7de804cf33a234fe
Gage <gschaffer@basecampcodeacademy.org>
2025-03-13 18:42:53 -0500

    Removed all instances of "example.html". Added proper landing pages for each app.

--- projectpinewood/blog/views.py
+++ projectpinewood/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog_landing_view(request):
-    return render(request, "blog/example.html")
+    return render(request, "blog/landing.html")
--- projectpinewood/storefront/views.py
+++ projectpinewood/storefront/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def store_landing_view(request):
-    return render(request, 'storefront/example.html')
+    return render(request, 'storefront/landing.html')
