f5a11ea0bd282b7aed7e8ece29b02a21c3ec12bd
Gage <gschaffer@basecampcodeacademy.org>
2025-03-13 18:33:14 -0500

    Added view function in each app. Added index.html for homepage

--- projectpinewood/blog/views.py
+++ projectpinewood/blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog_landing_view(request):
+    return render(request, "blog/example.html")
--- projectpinewood/storefront/views.py
+++ projectpinewood/storefront/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def store_landing_view(request):
+    return render(request, 'storefront/example.html')
