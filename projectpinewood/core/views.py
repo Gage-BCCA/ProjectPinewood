9fc48fbbbe0c91b39c1599d87dc8a63f45f7ff42
Gage <gschaffer@basecampcodeacademy.org>
2025-03-13 23:17:15 -0500

    Added about page. Added global base template. Extended all existing template from global base template.

--- projectpinewood/core/views.py
+++ projectpinewood/core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage_view(request):
     return render(request, "core/index.html")
+
+def about_view(request):
+    return render(request, "core/about.html")
