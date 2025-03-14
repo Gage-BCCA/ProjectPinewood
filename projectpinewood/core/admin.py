15a21f04d7f0807962a090609ce77b8ac5c3f189
Gage <gschaffer@basecampcodeacademy.org>
2025-03-13 23:41:50 -0500

    Created Subscriber model and InterestTag model. Registered in admin panel

--- projectpinewood/core/admin.py
+++ projectpinewood/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import InterestTags, Subscriber
 
 # Register your models here.
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    fields = ["title", "subtitle", "body", "status"]
+
+@admin.register(InterestTags)
+class InterestTagsAdmin(admin.ModelAdmin):
+    pass
--- projectpinewood/core/models.py
+++ projectpinewood/core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models import ManyToManyField
+
 
 # Create your models here.
+class InterestTags(models.Model):
+    """
+    Tags set during Newsletter sign up.
+    Has a many-to-many relationship with the
+    Subscriber model to allow for better insights
+    on what content a user wants to see.
+    """
+    interest_name = models.CharField(max_length=255)
+
+
+class Subscriber(models.Model):
+    """
+    Generic newsletter sign up model
+    """
+    email = models.EmailField()
+    first_name = models.CharField(max_length=100)
+    date_subscribed = models.DateTimeField(auto_now_add=True)
+    newsletter_delivered = models.IntegerField(default=0)
+    interests = ManyToManyField(InterestTags)
+
