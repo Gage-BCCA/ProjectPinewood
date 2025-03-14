6823500579d85b50e4dbc2a53adb0adaf22aff5b
Gage <gschaffer@basecampcodeacademy.org>
2025-03-13 20:16:30 -0500

    Added tests packages to each app. Wrote simple tests for Post model

---
+++ projectpinewood/blog/tests/test_models.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from blog.models import PostStatus, Post
+
+
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        PostStatus.objects.create(status="New")
+
+    def test_post_status_creation(self):
+        PostStatus.objects.create(status="Testing")
+        self.assertIsNotNone(PostStatus.objects.get(status="Testing"))
+
+    def test_post_creation(self):
+        Post.objects.create(
+            title="Article 1",
+            subtitle="The Best Article",
+            body="Just playing around with my bros",
+            status=PostStatus.objects.get(status="New"),
+        )
+
+        target_post = Post.objects.get(title="Article 1")
+        self.assertIsNotNone(target_post)
+        self.assertIsInstance(target_post, Post)
+        self.assertEqual(target_post.title, "Article 1")
+
+    def test_post_creation_slugify(self):
+        target_post = Post.objects.create(
+            title="Article 1",
+            subtitle="The Best Article",
+            body="Just playing around with my bros",
+            status=PostStatus.objects.get(status="New"),
+        )
+        self.assertEqual(target_post.slug, "article-1")
