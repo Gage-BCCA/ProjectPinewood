f56450cc86bb1ecb92bd5be82e893d0ecd560ff0
Gage Schaffer <gschaffer@basecampcodingacademy.org>
2025-03-16 23:18:39 -0500

    Added tag model. Added tags field on posts. Commented model classes.

--- projectpinewood/blog/admin.py
+++ projectpinewood/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, PostStatus
+from .models import Post, PostStatus, Tag
 
 # Register your models here.
 @admin.register(Post)
@@ -8,4 +8,8 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(PostStatus)
 class PostStatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- projectpinewood/blog/models.py
+++ projectpinewood/blog/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.utils.text import slugify
 
+class Tag(models.Model):
+    """Simple tag that helps identify what topics a post covers"""
+    tag = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.tag
+
 
 class PostStatus(models.Model):
+    """A status model that's used internally
+
+    Helps determine which posts have been published, or which ones are still
+    in progress, soft-deleted, etc.
+    """
     status = models.CharField(max_length=255)
 
     def __str__(self):
@@ -11,6 +23,12 @@ class PostStatus(models.Model):
 
 # Create your models here.
 class Post(models.Model):
+    """A model for all blog posts
+
+    Contains several standard fields for the post for display. Also
+    contains a few fields for internal use to determine how an article
+    is handled.
+    """
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
@@ -23,8 +41,13 @@ class Post(models.Model):
     featured_image = models.TextField(null=True)  # TODO: Add local image support
     view_count = models.BigIntegerField(default=0)
     status = models.ForeignKey('PostStatus', on_delete=models.CASCADE)
+    tags = models.ManyToManyField('Tag')
 
     def save(self, *args, **kwargs):
+        """Overridden save method that slugifies the article's title so it can be used in routing."""
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
