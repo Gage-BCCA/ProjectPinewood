41bc8bc07083e6d45162ad3e2e6c8185572c9fd8
Sean Jenkins <sjenkins@basecampcodingacademy.org>
2025-03-17 18:21:50 -0500

    added product model and adjusted featured product model.

--- projectpinewood/storefront/admin.py
+++ projectpinewood/storefront/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import *
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ('id', 'title', 'price', 'date_created')
+
+
 @admin.register(FeaturedProduct)
 class FeaturedProductAdmin(admin.ModelAdmin):
     fields = ('product', 'date_added', 'expiration_date')
-
-    def __str__(self):
-        return self.product.title #when product foreign key is created
--- projectpinewood/storefront/models.py
+++ projectpinewood/storefront/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+class Product(models.Model):
+    id = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    
+
 class FeaturedProduct(models.Model):
     product = models.OneToOneField(Product, on_delete=models.CASCADE)
     date_added = models.DateTimeField()#auto_now_add=True?
     expiration_date = models.DateTimeField(null=True, blank=True)
     
+    def __str__(self):
+        return self.product.title
