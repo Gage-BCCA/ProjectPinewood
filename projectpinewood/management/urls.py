f565f7ab667a63081a3a62765befc5b7fc7bf16f
Gage <gschaffer@basecampcodeacademy.org>
2025-03-22 14:45:17 -0500

    Big push. Lots of scaffolding for management app.

--- projectpinewood/management/urls.py
+++ projectpinewood/management/urls.py
@@ -4,9 +4,50 @@ from . import views
 
 urlpatterns = [
     path('', views.management_landing_view, name='management_landing'),
-    path('blog/', views.management_blog_view, name='blog_management'),
-    path('blog/add_post', views.management_blog_add_post_view, name='blog_add_post'),
-    path('storefront/', views.management_storefront_view, name='storefront_management'),
-    path('announcements/', views.management_announcements_view, name='announcements_management'),
-    path('newsletter/', views.management_newsletter_view, name='newsletter_management')
+
+    #=================================
+    # Blog URLs
+    #=================================
+    path('blog/', views.blog_overview, name='blog_management'),
+    path('blog/add', views.add_post_view, name='add_post'),
+    path('blog/delete', views.delete_post_view, name='delete_post'),
+    path('blog/edit', views.edit_post_view, name='edit_post'),
+    path('blog/all', views.all_posts_view, name='all_posts'),
+
+
+
+    #=================================
+    # Storefront URLs
+    #=================================
+    path('storefront/', views.storefront_overview, name='storefront_management'),
+    path('storefront/add', views.add_featured_product_view, name='add_featured_product'),
+    path('storefront/remove', views.remove_featured_product_view, name='remove_featured_product'),
+    path('storefront/all', views.all_featured_products_view, name='all_featured_products'),
+
+    #=================================
+    # Announcement URLs
+    #=================================
+    path('announcements/', views.announcements_overview, name='announcements_management'),
+    path('announcements/create', views.create_announcement_view, name='create_announcement'),
+    path('announcements/remove', views.remove_current_announcement_view, name='remove_announcement'),
+    path('announcements/all', views.all_announcements_view, name='all_announcements'),
+    path('announcements/edit', views.edit_announcement_view, name='edit_announcement'),
+
+    #=================================
+    # Newsletter URLs
+    #=================================
+    path('newsletter/', views.newsletter_overview, name='newsletter_management'),
+    path('newsletter/add', views.add_email_view, name='add_email'),
+    path('newsletter/remove', views.remove_email_view, name='remove_email'),
+    path('newsletter/all', views.all_emails_view, name='all_emails'),
+
+
+    #=================================
+    # Gallery URLs
+    #=================================
+    path('gallery/', views.gallery_overview, name='gallery_management'),
+    path('gallery/add', views.add_picture_view, name='add_picture'),
+    path('gallery/remove', views.remove_picture_view, name='remove_picture'),
+    path('gallery/all', views.all_pictures_view, name='add_picture'),
+    path('gallery/edit', views.edit_picture_view, name='edit_picture'),
 ]
--- projectpinewood/management/views.py
+++ projectpinewood/management/views.py
@@ -4,17 +4,86 @@ from django.shortcuts import render
 def management_landing_view(request):
     return render(request, "management/landing.html")
 
-def management_blog_view(request):
+#=================================
+# Blog Views
+#=================================
+def blog_overview(request):
     return render(request, "management/blog/landing.html")
 
-def management_blog_add_post_view(request):
+def add_post_view(request):
     return render(request, "management/blog/add_post.html")
 
-def management_storefront_view(request):
+def edit_post_view(request):
+    return render(request, "management/blog/edit_post.html")
+
+def all_posts_view(request):
+    return render(request, "management/blog/all_posts.html")
+
+def delete_post_view(request):
+    return render(request, "management/blog/delete_post.html")
+
+#=================================
+# Storefront Views
+#=================================
+def storefront_overview(request):
     return render(request, "management/storefront/landing.html")
 
-def management_announcements_view(request):
+def add_featured_product_view(request):
+    return render(request, "management/storefront/add_featured_product.html")
+
+def remove_featured_product_view(request):
+    return render(request, "management/storefront/remove_featured_product.html")
+
+def all_featured_products_view(request):
+    return render(request, "management/storefront/all_featured_products.html")
+
+#=================================
+# Announcements Views
+#=================================
+def announcements_overview(request):
     return render(request, "management/announcements/landing.html")
 
-def management_newsletter_view(request):
+def all_announcements_view(request):
+    return render(request, "management/announcements/all_announcements.html")
+
+def create_announcement_view(request):
+    return render(request, "management/announcements/create_announcement.html")
+
+def remove_current_announcement_view(request):
+    return render(request, "management/announcements/remove_current_announcement.html")
+
+def edit_announcement_view(request):
+    return render(request, "management/announcements/edit_announcement.html")
+
+#=================================
+# Newsletter Views
+#=================================
+def newsletter_overview(request):
     return render(request, "management/newsletter/landing.html")
+
+def add_email_view(request):
+    return render(request, "management/newsletter/add_email.html")
+
+def remove_email_view(request):
+    return render(request, "management/newsletter/remove_email.html")
+
+def all_emails_view(request):
+    return render(request, "management/newsletter/all_emails.html")
+
+#=================================
+# Gallery Views
+#=================================
+def gallery_overview(request):
+    return render(request, "management/gallery/landing.html")
+
+def add_picture_view(request):
+    return render(request, "management/gallery/add_picture.html")
+
+def remove_picture_view(request):
+    return render(request, "management/gallery/remove_picture.html")
+
+def all_pictures_view(request):
+    return render(request, "management/gallery/all_pictures.html")
+
+def edit_picture_view(request):
+    return render(request, "management/gallery/edit_picture.html")
